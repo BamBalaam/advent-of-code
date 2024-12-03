f61f1923644a5e0c50c94010a3d9d80350bce531
André Madeira Cortes <amadeiracortes@gmail.com>
2024-12-03 09:45:43 +0100

    Cleanup: 2024 - Day 1

--- 2024/day1/day1.py
+++ 2024/day1/day1.py
@@ -1,18 +1,13 @@
 def clean_and_separate_lists(lists):
     clean_lists = [line.strip().split("   ") for line in lists]
     left_list, right_list = map(list, zip(*clean_lists))
-    left_list = list(map(int, left_list))
-    right_list = list(map(int, right_list))
-    return left_list, right_list
+    return list(map(int, left_list)), list(map(int, right_list))
 
 def calculate_distances(left_list, right_list):
-    distances = list(map(lambda x, y: abs(x-y), sorted(left_list), sorted(right_list)))
-    return distances
+    return list(map(lambda x, y: abs(x-y), sorted(left_list), sorted(right_list)))
 
 def calculate_similarity_scores(left_list, right_list):
-    scores = list(map(lambda x, y: x * right_list.count(x), left_list, right_list))
-    return scores
-
+    return list(map(lambda x, y: x * right_list.count(x), left_list, right_list))
 
 if __name__ == '__main__':
     print("Day 1: Historian Hysteria\n")
@@ -20,17 +15,15 @@ if __name__ == '__main__':
     print("Example solutions:\n")
     with open("day1_example.txt", 'r') as file:
         lines = file.readlines()
-    left_list, right_list = clean_and_separate_lists(lines)
-    distances = calculate_distances(left_list, right_list)
+    distances = calculate_distances(*clean_and_separate_lists(lines))
     print("Sum of distances: " + str(sum(distances)))
-    scores = calculate_similarity_scores(left_list, right_list)
+    scores = calculate_similarity_scores(*clean_and_separate_lists(lines))
     print("Total similarity score: " + str(sum(scores)))
 
     print("\nInput solutions:\n")
     with open("day1_input.txt", 'r') as file:
         lines = file.readlines()
-    left_list, right_list = clean_and_separate_lists(lines)
-    distances = calculate_distances(left_list, right_list)
+    distances = calculate_distances(*clean_and_separate_lists(lines))
     print("Sum of distances: " + str(sum(distances)))
-    scores = calculate_similarity_scores(left_list, right_list)
+    scores = calculate_similarity_scores(*clean_and_separate_lists(lines))
     print("Total similarity score: " + str(sum(scores)))
